f4f345078448c36214f602e7dcf8f09e3815a3b6
elerac <maenosuke079@gmail.com>
2019-05-09 13:45:25 +0900

    Create easyHDR.py

---
+++ easyHDR.py
@@ -0,0 +1,36 @@
+import tisgrabber as IC
+import cv2
+import numpy as np
+
+#Create the camera object
+Camera = IC.TIS_CAM()
+#Print all devices
+Devices = Camera.GetDevices()
+for i in range(len( Devices )):
+    print( str(i) + " : " + str(Devices[i]))
+#Show dialog
+Camera.ShowDeviceSelectionDialog()
+#No devices
+if Camera.IsDevValid() != 1:
+	print( "No device selected")
+	exit()
+
+#Capture Start
+while True:
+    #HDR capture
+    hdr = capture(Camera, ExposureTime=0.0333, GainValue=16, average=2, HDR=True)
+    tonemap = cv2.createTonemapDurand(gamma=2.2)
+    res = tonemap.process(hdr.copy())
+    res_8bit = np.clip(res*255, 0, 255).astype(np.uint8)
+    img_hdr = res_8bit
+
+    #LDR capture
+    img_ldr = capture(Camera, ExposureTime=0.0333, GainValue=16, average=1, HDR=False)
+
+    cv2.imshow("ldr", img_ldr)
+    cv2.imshow("hdr", img_hdr)
+    key = cv2.waitKey(10)
+    if key == ord("q"):
+        break
+   
+cv2.destroyAllWindows()
